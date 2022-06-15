6d6ffa6f4e054aa5653b9e9993f797787b20e0f0
ASAFMI <asaf@mi.com>
2022-06-15 09:43:06 +0300

    dataset loading and organizing

--- HW1/dataloading.py
+++ HW1/dataloading.py
@@ -1,16 +1,16 @@
-from consts import *
+# from consts import *
 from scipy.sparse import csr_matrix
 
 
 def load_raw_dataset(txtFilePath):
     with open(txtFilePath) as f:
         lines = f.readlines()
-    dataset = [line.strip().split(sep="\t") for line in lines]
-    return [item[0] for item in dataset], [item[1] for item in dataset]
+    dataset = [line.strip().split(sep=" ") for line in lines if not(line in ['\n', '. . O\n'])]
+    return [item[0] for item in dataset], [item[1] for item in dataset], [item[2] for item in dataset]
 
 
-def convert_raw_to_features():
-    pass
+def convert_raw_to_features(lWords):
+    return [word[0].isupper() for word in lWords]
 
 
 def get_dataset():
--- HW1/main.py
+++ HW1/main.py
@@ -1,4 +1,6 @@
-
+from dataloading import *
 
 if __name__ == '__main__':
-    pass
+    x_train, feature_train, y_train = load_raw_dataset("data\\train.txt") #TODO: feature_train >> one hot encoding
+    capital_feature = convert_raw_to_features(x_train) #TODO: capital_feature boolian to 1,0
+    feature_train = [list(x) for x in zip(feature_train, capital_feature)]
