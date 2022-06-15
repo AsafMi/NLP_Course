7d52fa139152eed4966cea6fee02ba601d39fab5
ASAFMI <asaf@mi.com>
2022-06-15 14:04:29 +0300

    prepare dataset with OneHotEncoder

--- HW1/dataloading.py
+++ HW1/dataloading.py
@@ -6,7 +6,7 @@ def load_raw_dataset(txtFilePath):
     with open(txtFilePath) as f:
         lines = f.readlines()
     dataset = [line.strip().split(sep=" ") for line in lines if not(line in ['\n', '. . O\n'])]
-    return [item[0] for item in dataset], [item[1] for item in dataset], [item[2] for item in dataset]
+    return [item[0] for item in dataset], [item[1] for item in dataset], [[item[2]] for item in dataset]
 
 
 def convert_raw_to_features(lWords):
--- HW1/main.py
+++ HW1/main.py
@@ -1,6 +1,9 @@
 from dataloading import *
+from sklearn.preprocessing import OneHotEncoder
 
 if __name__ == '__main__':
-    x_train, feature_train, y_train = load_raw_dataset("data\\train.txt") #TODO: feature_train >> one hot encoding
-    capital_feature = convert_raw_to_features(x_train) #TODO: capital_feature boolian to 1,0
+    x_train, feature_train, y_train = load_raw_dataset("data\\train.txt")
+    capital_feature = convert_raw_to_features(x_train)
     feature_train = [list(x) for x in zip(feature_train, capital_feature)]
+    feature_train = OneHotEncoder().fit_transform(feature_train)
+    y_train = OneHotEncoder(drop='if_binary').fit_transform(y_train)
