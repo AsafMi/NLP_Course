4ab48d966ea7182e047bb0e1d1d7c0e0e4e0d730
ASAFMI <asaf@mi.com>
2022-06-20 15:41:29 +0300

    writing results in test.txt

--- HW1/main.py
+++ HW1/main.py
@@ -8,8 +8,10 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+from tqdm import tqdm
 
 start_time = time.time()
+plotConfusionMatrix = False
 
 if __name__ == '__main__':
     # ------------------------------------------------------------------
@@ -38,7 +40,7 @@ if __name__ == '__main__':
     # ------------------------------------------------------------------
     model = SVC()  # TODO:GRIDSEARCH
     model.fit(feature_train, y_train)  # fitting the model on the training data
-    print(f"SVM model was trained\nElapse time: {time.time() - start_time:.02f} sec")
+    print(f"SVM model was trained\nElapsed time: {time.time() - start_time:.02f} sec")
     print(f"Train score: {model.score(feature_train, y_train):.03f}")
     print(f"Eval score: {model.score(feature_val, y_val):.03f}")
 
@@ -47,8 +49,9 @@ if __name__ == '__main__':
     cm = confusion_matrix(y_val, predictions, labels=model.classes_)  # building a confusion matrix
     disp = ConfusionMatrixDisplay(confusion_matrix=cm,
                                   display_labels=model.classes_)
-    disp.plot()
-    plt.show()
+    if plotConfusionMatrix:
+        disp.plot()
+        plt.show()
 
     # Saving bad classification for review
     confused_data = [word + f" , {y_val[idx]}\n" for idx, word in enumerate(x_val) if y_val[idx] != predictions[idx]]
@@ -58,10 +61,40 @@ if __name__ == '__main__':
     file.close()
 
     # create "competitive.txt" for substitution
+    # train on a bigger dataset (train + eval)
+    x_train, feature_train, y_train = load_raw_dataset("data\\train.txt")
+    x_val, feature_val, y_val = load_raw_dataset("data\\eval.txt")
+    x_train.extend(x_val)
+    feature_train.extend(feature_val)
+    y_train.extend(y_val)
+
+    x_encoder = OneHotEncoder(handle_unknown="ignore").fit(feature_train)
+    feature_train = x_encoder.transform(feature_train)
+
+    y_encoder = OneHotEncoder(drop="if_binary").fit(y_train)
+    y_train = np.array(y_encoder.transform(y_train).toarray()).ravel()
+
+    model = SVC()
+    model.fit(feature_train, y_train)
     # open both files
+    open('competitive.txt', 'w').close()
     with open('data/test.txt', 'r') as firstfile, open('competitive.txt', 'a') as secondfile:
         # read content from test file
-        for line in firstfile:
+        for line in tqdm(firstfile):
+            data = line.strip().split(sep=" ")
+            if len(data) < 2:
+                secondfile.write(line)
+                continue
+            x = data[0]
+            feature = data[1]
             # append content to second file
-            if line == ". .":
-                secondfile.write(line + " O\n")
+            if x in [".", ",", "-"]:
+                secondfile.write(x + " " + feature + " " + "O\n")
+                continue
+            else:
+                capital_feature = x.isupper()
+                x_test = [x, feature, capital_feature]
+                x_test = x_encoder.transform([x_test])
+                y_test = model.predict(x_test)
+                label = y_encoder.inverse_transform([y_test])
+                secondfile.write(x + " " + feature + " " + f"{label[0][0]}\n")
